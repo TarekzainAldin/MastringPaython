e061d81d8fe320be81172209ab429cd8150d1315
Tarek <tarekzindin@gmail.com>
2026-05-20 12:55:58 +0200

    con

---
+++ 5-VariablesPartTwo.py
@@ -0,0 +1,17 @@
+# ---------------
+# -- Variables --
+# ---------------
+# Source Code : Original Code You Write it in Computer
+# Translation : Converting Source Code Into Machine Language
+# Compilation : Translate Code Before Run Time
+# Run-Time : Period App Take To Executing Commands
+# Interpreted : Code Translated On The Fly During Execution
+# --------------------------------------------------------
+
+# Reserved Words
+
+help("keywords")
+a, b, c = 1, 2, 3
+print(a)  # Output: 1
+print(b)  # Output: 2       
+print(c)  # Output: 3
